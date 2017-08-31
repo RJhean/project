0e7b44c3e992a1a91de942b8be700925af6d67c6
RJhean <jhean.rp@gmail.com>
2017-08-30 21:09:14 -0500

    false

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1czb%z7if@j^!*)zefsrl9qd2nzoa^)t%hms^-^o=-li83uw+('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
